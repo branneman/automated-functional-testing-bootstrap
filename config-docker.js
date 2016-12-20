f3c86d0fc17b5f02725622bb65514d33228c9b4c
Bran van der Meer <branmovic@gmail.com>
2016-12-20 20:18:47 +0100

    Added config-local

---
+++ config-docker.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const exec = require('child_process').execSync;
+const commonConfig = require('./config.js');
+
+exports.config = Object.assign({}, commonConfig, {
+
+    capabilities: [{ browserName: 'chrome' }],
+
+    /**
+     * Start Selenium Grid
+     */
+    onPrepare: function(config, capabilities) {
+        exec('docker run --restart=always -d -p 4444:4444 --name selenium-hub "selenium/hub:3.0.0-dubnium"');
+        for (let i = 0; i < commonConfig.maxInstances; i++) {
+            exec(`docker run --restart=always -d --link selenium-hub:hub --name=chromedriver-${i} "selenium/node-chrome:3.0.0-dubnium"`);
+        }
+    },
+
+    /**
+     * Stop Selenium Grid
+     */
+    onComplete: function(exitCode) {
+        exec('docker rm -f selenium-hub');
+        for (let i = 0; i < commonConfig.maxInstances; i++) {
+            exec(`docker rm -f chromedriver-${i}`);
+        }
+    }
+
+});
--- config-local.js
+++ config-local.js
@@ -1,29 +1,27 @@
 'use strict';
 
-const exec = require('child_process').execSync;
 const commonConfig = require('./config.js');
+const selenium = require('selenium-standalone');
+
+let seleniumChild;
 
 exports.config = Object.assign({}, commonConfig, {
 
     capabilities: [{ browserName: 'chrome' }],
 
-    /**
-     * Start Selenium Grid
-     */
-    onPrepare: function(config, capabilities) {
-        exec('docker run --restart=always -d -p 4444:4444 --name selenium-hub selenium/hub:3.0.0-dubnium');
-        for (let i = 0; i < commonConfig.maxInstances; i++) {
-            exec(`docker run --restart=always -d --link selenium-hub:hub --name=chromedriver-${i} selenium/node-chrome:3.0.0-dubnium`);
-        }
+    onPrepare: function() {
+        selenium.start((err, child) => {
+            if (err) {
+                console.log(err);
+                return process.exit();
+            }
+            seleniumChild = child;
+        });
     },
 
-    /**
-     * Stop Selenium Grid
-     */
-    onComplete: function(exitCode) {
-        exec('docker rm -f selenium-hub');
-        for (let i = 0; i < commonConfig.maxInstances; i++) {
-            exec(`docker rm -f chromedriver-${i}`);
+    onComplete: function() {
+        if (seleniumChild) {
+            seleniumChild.kill();
         }
     }
 
